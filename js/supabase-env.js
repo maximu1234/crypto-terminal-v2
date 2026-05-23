38435f46e2c6997e813cc882f28f3ae8888c561c
maximu1234 <128886718+maximu1234@users.noreply.github.com>
2026-05-23 18:50:45 +0700

    Create supabase-env.js

---
+++ js/supabase-env.js
@@ -0,0 +1,2 @@
+export const SUPABASE_URL = "https://ehygysphfsnluegeycjx.supabase.co";
+export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVoeWd5c3BoZnNubHVlZ2V5Y2p4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk1MjE2NTYsImV4cCI6MjA5NTA5NzY1Nn0.QhKpETjLuIpRC3v9kUrsxoXX17YXPiZVfgEGNP93ubw";
